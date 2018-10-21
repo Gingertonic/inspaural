97e86b7a5f093cd06751f064548dfa21c3eb4086
Gingertonic <bethmschofield@gmail.com>
2018-10-21 10:39:58 +0100

    Testing react-sound

--- inspaural-api/inspaural-client/src/components/Quotes.js
+++ inspaural-api/inspaural-client/src/components/Quotes.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Sound from 'react-sound';
+
 
 const Quotes = (props) => {
-  const renderQuotes = props.quotes.map(quote => <img className="quote" id={quote.id} key={quote.id} src={quote.imageUrl}/>)
+  const handleOnMouseEnter = (e) => {
+    console.log(e.target)
+  }
+
+  const renderQuotes = props.quotes.map(quote => <img className="quote" onMouseOver={(e) => handleOnMouseEnter(e)} id={quote.id} key={quote.id} audiourl={quote.audioUrl} src={quote.imageUrl}/>)
+
   return (
     <div className="quotes-container">
       {renderQuotes}
+      <Sound url="https://www.looperman.com/media/loops/159051/looperman-l-0159051-0127180-minor2go-my-3000th-loop-part-1.mp3" autoload={true} playStatus="PLAYING"/>
     </div>
   )
 }
